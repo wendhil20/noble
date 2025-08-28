ecfe6b48a7f32c3fb7675def8fdedd1cf41d3c67
lking0641 <lking0641@gmail.com>
2025-08-28 15:47:27 +0800

    role update

--- admin/logistic_management/logistics_dashboard.php
+++ admin/logistic_management/logistics_dashboard.php
@@ -5,7 +5,7 @@ session_start();
 
 include '../../connection/connect.php';
 require_once '../role/roleaccount.php';
-require_role(['productspecialist', 'superadmin', 'sales', 'warehouse']);
+require_role(['productspecialist', 'superadmin', 'sales', 'warehouse', 'logistic']);
 
 if (!isset($_SESSION['noble_user'])) {
     header("Location: ../../loginpage/index.php");
